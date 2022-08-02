c2d5d588cf124034dddd31f0472d89b27ff5bb5f
caritoespicaita <caritoespicaita@hotmail.com>
2022-08-02 16:10:10 -0500

    upload files

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  /**
+     * Returns the attributes in the following format: amount currency_name (currency_code).
+     */
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
