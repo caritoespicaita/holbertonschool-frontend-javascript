ebd2fc3cd0d55a13fbdb3554a7980210d7d0e15a
caritoespicaita <caritoespicaita@hotmail.com>
2022-08-02 11:53:33 -0500

    upload task

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
-  const array = ['Guillaume', '1', 'San Francisco'];
-  const array1 = ['James', '2', 'San Francisco'];
-  const array2 = ['Serena', '5', 'San Francisco'];
+  const array = ['Guillaume', '1', 'San Francisco',
+    'James', '2', 'San Francisco',
+    'Serena', '5', 'San Francisco'];
 
   return ([{ id: array[1], firstName: array[0], location: array[2] },
-    { id: array1[1], firstName: array1[0], location: array1[2] },
-    { id: array2[1], firstName: array2[0], location: array2[2] }]);
+    { id: array[3], firstName: array[4], location: array[5] },
+    { id: array[6], firstName: array[7], location: array[8] }]);
 }
