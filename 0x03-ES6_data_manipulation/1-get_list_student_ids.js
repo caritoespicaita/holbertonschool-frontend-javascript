db9b187dbb2a7beeade1d697e9e0c65c30dc0704
caritoespicaita <caritoespicaita@hotmail.com>
2022-08-03 12:09:21 -0500

    upload task 1.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,7 @@
-export default function getListStudentIds() {
-
-}
+export default function getListStudentIds(ids) {
+    if (!Array.isArray(ids)) {
+        return [];
+    }
+    const id = ids.map(item => item.id);
+    return id;
+  }
